a7441fd57fd38146edb0659ad179ca92aaa9304c
victor-afonso <victor.asilva@bandtec.com.br>
2022-04-23 01:01:44 -0300

    fiixed-jar2

--- banco-monitech/src/main/java/br/com/bandtec/banco/teste/Connection.java
+++ banco-monitech/src/main/java/br/com/bandtec/banco/teste/Connection.java
@@ -13,12 +13,12 @@ public class Connection {
         this.datasource = new BasicDataSource();
 
 //        this.datasource.setDriverClassName("org.h2.Driver");
-        this.datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        //this.datasource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        //this.datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        this.datasource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
 //        this.datasource.setUrl("jdbc:h2:file:./meu_banco");
-        this.datasource.setUrl("jdbc:mysql://localhost:3306/monitech?useTimezone=true&serverTimezone=UTC");
-        //this.datasource.setUrl("jdbc:sqlserver://svr-monitech.database.windows.net:1433;database=bd-Monitech;user=admin-Monitech@svr-monitech;password=2ads#grupo3;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30");
+        //this.datasource.setUrl("jdbc:mysql://localhost:3306/monitech?useTimezone=true&serverTimezone=UTC");
+        this.datasource.setUrl("jdbc:sqlserver://svr-monitech.database.windows.net:1433;database=bd-Monitech;user=admin-Monitech@svr-monitech;password=2ads#grupo3;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30");
 
         // usuário padrao H2
 //        this.datasource.setUsername("sa");
